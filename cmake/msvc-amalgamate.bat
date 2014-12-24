8bbd29816315f8045f443fcc3e5faa7403faf9e9
Nick Landry <nick.landry@gmail.com>
2014-12-23 23:11:48 -0500

    Fix up msvc-amalgamate
    You know... so it works and stuff.

--- cmake/msvc-amalgamate.bat
+++ cmake/msvc-amalgamate.bat
@@ -1,9 +1,19 @@
 set ROOTDIR=%CD%
 
-pushd out\external-deps\targets\Windows\x86_64
+pushd out\external-deps\targets\Windows\x86_64\Debug
 mkdir tmp
 move *.lib tmp
 
 LIB.EXE /OUT:gameplay-deps.lib tmp\BulletCollision.lib tmp\BulletDynamics.lib tmp\GLEW.lib tmp\LinearMath.lib tmp\lua.lib tmp\ogg.lib tmp\OpenAL32.lib tmp\png.lib tmp\tinyxml2.lib tmp\vorbis.lib tmp\vorbisenc.lib tmp\vorbisfile.lib tmp\z.lib
 
 rmdir /s /q "tmp\"
+popd
+
+pushd out\external-deps\targets\Windows\x86_64\Release
+mkdir tmp
+move *.lib tmp
+
+LIB.EXE /OUT:gameplay-deps.lib tmp\BulletCollision.lib tmp\BulletDynamics.lib tmp\GLEW.lib tmp\LinearMath.lib tmp\lua.lib tmp\ogg.lib tmp\OpenAL32.lib tmp\png.lib tmp\tinyxml2.lib tmp\vorbis.lib tmp\vorbisenc.lib tmp\vorbisfile.lib tmp\z.lib
+
+rmdir /s /q "tmp\"
+popd
