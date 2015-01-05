d636268df1d851184be3e1657e962cdc766216d7
Nick Landry <nick.landry@gmail.com>
2015-01-04 20:34:55 -0500

    Lowercase all platform names
    Also change macos to macosx

--- cmake/ios.toolchain.cmake
+++ cmake/ios.toolchain.cmake
@@ -145,10 +145,10 @@ set (CMAKE_OSX_SYSROOT ${CMAKE_IOS_SDK_ROOT} CACHE PATH "Sysroot used for iOS su
 # NOTE: Currently both ARCHS_STANDARD_32_BIT and ARCHS_UNIVERSAL_IPHONE_OS set armv7 only, so set both manually
 if (${IOS_PLATFORM} STREQUAL "OS")
 	set (IOS_ARCH armv7 armv7s arm64)
-	set(LIBRARY_OUTPUT_PATH "${PROJECT_SOURCE_DIR}/out/external-deps/libs/iOS/arm")
+	set(LIBRARY_OUTPUT_PATH "${PROJECT_SOURCE_DIR}/out/external-deps/libs/ios/arm")
 else (${IOS_PLATFORM} STREQUAL "OS")
 	set (IOS_ARCH i386 x86_64)
-	set(LIBRARY_OUTPUT_PATH "${PROJECT_SOURCE_DIR}/out/external-deps/libs/iOS/x86")
+	set(LIBRARY_OUTPUT_PATH "${PROJECT_SOURCE_DIR}/out/external-deps/libs/ios/x86")
 endif (${IOS_PLATFORM} STREQUAL "OS")
 
 set (CMAKE_OSX_ARCHITECTURES ${IOS_ARCH} CACHE string  "Build architecture for iOS")
